3d8504c37ada32904b65581fabd6ce3d193e34ba
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-15 11:16:42 +0000

    [fix]Topページ　表示内容修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,11 @@
 class HomesController < ApplicationController
   def top
+    if user_signed_in?
     user_ids=current_user.followings.pluck(:id)
     @mysteries=Mystery.where(user_id: user_ids).order(created_at: :desc)
+    else
+      @mysteries=Mystery.all
+    end
   end
 
   def about
