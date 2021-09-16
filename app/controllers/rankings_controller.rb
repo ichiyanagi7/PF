544455031dc22916045a2400d3670ae771c53226
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-16 07:42:30 +0000

    [add]ランキング投稿数

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,7 @@
 class RankingsController < ApplicationController
   def index
-    @mysteries=Mystery.includes(:favorited_users).sort {|a,b|b.favorited_users.size <=> a.favorited_users.size}
-    @users=User.all
+    # Mystery モデルからfavorited_usersの多い順に並び替え、先頭から10個取り出す
+    @mysteries=Mystery.includes(:favorited_users).sort {|a,b|b.favorited_users.size <=> a.favorited_users.size}.first(10)
+    @users=User.includes(:mysteries).sort{|a,b|b.mysteries.size <=> a.mysteries.size}.first(10)
   end
 end
