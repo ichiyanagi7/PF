df11afdb1e9fdbb0df02535513feae41a84b50d8
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-16 08:09:15 +0000

    [fix]カード高さ指定

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,6 +2,8 @@ class RankingsController < ApplicationController
   def index
     # Mystery モデルからfavorited_usersの多い順に並び替え、先頭から10個取り出す
     @mysteries=Mystery.includes(:favorited_users).sort {|a,b|b.favorited_users.size <=> a.favorited_users.size}.first(10)
+
+    # Userモデルからmysteryの多い順に10個取り出す
     @users=User.includes(:mysteries).sort{|a,b|b.mysteries.size <=> a.mysteries.size}.first(10)
   end
 end
