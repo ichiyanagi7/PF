754a057999e7c227919a1536c158b7e337324642
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-16 05:14:00 +0000

    [add]非同期通信_お気に入り

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,19 +6,20 @@ class FavoritesController < ApplicationController
     @genres=Genre.all
   end
 
-
+  # 非同期通信
   def create
-    mystery=Mystery.find(params[:mystery_id])
-    favorite=current_user.favorites.new(mystery_id: mystery.id)
+    @mystery=Mystery.find(params[:mystery_id])
+    favorite=current_user.favorites.new(mystery_id: @mystery.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
+  # 非同期通信
   def destroy
-    mystery=Mystery.find(params[:mystery_id])
-    favorite=current_user.favorites.find_by(mystery_id: mystery.id)
+    @mystery=Mystery.find(params[:mystery_id])
+    favorite=current_user.favorites.find_by(mystery_id: @mystery.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
 end
