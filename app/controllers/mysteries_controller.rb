0bd7a26382929bde93594618c114310f813a882a
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-19 04:02:11 +0000

    [add]タグ検索

--- app/controllers/mysteries_controller.rb
+++ app/controllers/mysteries_controller.rb
@@ -2,7 +2,11 @@ class MysteriesController < ApplicationController
   def index
     @mysteries=Mystery.all.order(created_at: :desc)
     @genres=Genre.all
-    @tags=Mystery.tag_counts_on(:tags)
+    @tags=Mystery.tag_counts_on(:tags).order("count DESC")
+    # タグ検索用
+    if @tag=params[:tag]
+      @tag_mysteries=Mystery.tagged_with(params[:tag])
+    end
   end
 
   def show
