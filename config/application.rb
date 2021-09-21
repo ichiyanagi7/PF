188f1bbaf8b700fcca05b59e9a0cca893e68fd65
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-09-21 11:48:49 +0000

    [fix]バッチ機能修正

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Mysterio
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
