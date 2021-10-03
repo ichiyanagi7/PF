aab7de90dd45054cb4b9c2c9053ee9e73497a908
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-10-03 04:33:28 +0000

    [add]GoogleNaturalLanguage API追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ class CommentsController < ApplicationController
     comment=current_user.comments.new(comment_params)
     comment.mystery_id=mystery.id
     comment_count=Comment.where(mystery_id: params[:mystery_id],user_id: current_user.id).count
+    # APIスコア付け
+    comment.score=Language.get_data(comment_params[:comment])
     if comment_count < 1
       if comment.save
         redirect_to mystery_path(mystery),notice: "レビューを投稿しました!"
@@ -24,6 +26,7 @@ class CommentsController < ApplicationController
 
   def update
     comment=Comment.find_by(mystery_id: params[:mystery_id],id: params[:id])
+    comment.score=Language.get_data(comment_params[:comment])
     comment.update(comment_params)
     redirect_to mystery_path(comment.mystery)
   end
@@ -37,7 +40,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:user_id,:mystery_id,:comment,:rate)
+    params.require(:comment).permit(:user_id,:mystery_id,:comment,:rate,:score)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_083601) do
+ActiveRecord::Schema.define(version: 2021_10_03_040317) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_083601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "rate", default: 0.0, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "favorites", force: :cascade do |t|
